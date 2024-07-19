a0297982a333dfe10dc9c28331f301674a0975ed
HoneyHazard <zeromemory@gmail.com>
2024-07-19 16:27:30 -0700

    wip

--- run_ola002.sh
+++ run_ola002.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# /usr/bin/python3 ./app/main.py --ola002 --qlc-workspace=qlcplus_workspace/ola002.qxw
-/usr/bin/python3 ./app/main.py --tooltip --ola002 --qlc-workspace=qlcplus_workspace/ola002.qxw --qlc-fixture=qlcplus_gruolin_olaalite_a001_a002/ola002_34ch.qxf
+python ./app/main.py --tooltip --ola002 --qlc-workspace=qlcplus_workspace/ola002.qxw --qlc-fixture=qlcplus_gruolin_olaalite_a001_a002/ola002_34ch.qxf
